4f8d3fb8c8bb11aa2f19e856e6bcb6b1beb6af44
Andrea Macaro <andreamac2000@gmail.com>
2022-05-29 23:49:37 +0200

    implementato passport

--- app/app.js
+++ app/app.js
@@ -1,30 +1,98 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
+const express = require('express'),
+	session = require('express-session'),
+	passport = require('passport'),
+	SpotifyStrategy = require('passport-spotify').Strategy,
+	path = require('path'),
+	cookieParser = require('cookie-parser');
 
-const authRouter = require('./routes/auth');
+require('dotenv').config();
+
+const SCOPE = ['user-read-private', 'user-read-email'];
+
+passport.serializeUser(function (user, done) {
+	done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+	done(null, obj);
+});
+
+passport.use(
+	new SpotifyStrategy(
+		{
+			clientID: process.env.CLIENT_ID,
+			clientSecret: process.env.CLIENT_SECRET,
+			callbackURL: process.env.REDIRECT_URI,
+		},
+		function (accessToken, refreshToken, expires_in, profile, done) {
+			// asynchronous verification, for effect...
+			process.nextTick(function () {
+				// To keep the example simple, the user's spotify profile is returned to
+				// represent the logged-in user. In a typical application, you would want
+				// to associate the spotify account with a user record in your database,
+				// and return that user instead.
+				return done(null, profile);
+			});
+		}
+	)
+);
 
 const app = express();
 const DEFAULT_PORT = '8888';
 
-app.use(express.static(path.join(__dirname, 'public')))
-    .use(cookieParser())
-    .use('/auth', authRouter);
-
 app.set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'pug');
+	.set('view engine', 'pug')
+	.use(express.static(path.join(__dirname, 'public')))
+	.use(express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist')))
+	.use('/css', express.static(path.join(__dirname, 'node_modules', 'bootstrap-icons', 'font')))
+	.use(cookieParser())
+	.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }))
+	.use(passport.initialize())
+	.use(passport.session());
 
-app.get('/', (_req, res) => {
-    const NODE = process.env.NODE_ENV || 'dev-local';
-    const INSTANCE = process.env.INSTANCE || 'node';
-    const PORT = process.env.PORT || DEFAULT_PORT;
 
-    res.render('index', {
-        title: 'Docker with Nginx and Express',
-        node: NODE,
-        instance: INSTANCE,
-        port: PORT
-    });
+app.get('/', (req, res) => {
+	const NODE = process.env.NODE_ENV || 'dev-local';
+	const INSTANCE = process.env.INSTANCE || 'node';
+	const PORT = process.env.PORT || DEFAULT_PORT;
+
+	res.render('index', {
+		title: 'Docker with Nginx and Express',
+		node: NODE,
+		instance: INSTANCE,
+		port: PORT,
+		user: req.user
+	});
 });
 
+app.get('/account', ensureAuthenticated, function (req, res) {
+	res.render('account', { user: req.user });
+	console.log(req.user._json);
+});
+
+app.get('/login', passport.authenticate('spotify', { scope: SCOPE }));
+
+app.get('/auth/callback', passport.authenticate('spotify', { failureRedirect: '/' }), function (req, res) {
+	let prevSession = req.session;
+	req.session.regenerate(() => {
+		Object.assign(req.session, prevSession);
+		res.redirect('/');
+	});
+}
+);
+
+app.get('/logout', function (req, res, next) {
+	req.logout(function (err) {
+		if (err) { return next(err); }
+		res.redirect('/');
+	});
+});
 app.listen(process.env.PORT || DEFAULT_PORT);
+
+function ensureAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	req.session.returnTo = req.originalUrl;
+	res.redirect('/login');
+}
